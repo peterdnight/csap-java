2d34f1767a6a3ced0c538dd9f604909f00dcc014
peterdnight <p.eternightingale@gmail.com>
2017-10-09 09:39:27 -0400

    servlet reference implementation

---
+++ csap-java-servlet/src/main/java/org/csap/sample/HelloServlet.java
@@ -0,0 +1,37 @@
+package org.csap.sample;
+
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "HelloServlet", urlPatterns = { "/showHelloServlet" })
+public class HelloServlet extends HttpServlet {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+
+		StringBuffer resultsBuff = new StringBuffer("\n Hello from " + this.getClass().getName() );
+		
+		System.out.println( new Date() + "\tDemo only - standard out should never be used, configure logging framework + upload");
+		
+		
+		request.setAttribute(SimpleJdbcServlet.RESULTS, resultsBuff) ;
+		// response.getWriter().println(resultsBuff);
+		request.getRequestDispatcher("/index.jsp").forward(request, response) ;
+	}
+}
